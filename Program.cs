fec95c9337567cb8d53c1dc2557a0433c0778e7f
Celvyn <yisusparley@gmail.com>
2020-04-15 01:23:46 -0600

    Se agrego un metodo para pedir la respuesta al usuario

--- Program.cs
+++ Program.cs
@@ -10,7 +10,10 @@ namespace MiniJuego
             Random numero = new Random();
             // Genera un numero aleatorio entre 0 y sin incluir el 101
             int numeroAleatorio = numero.Next(1, 101);
-            Console.WriteLine(numeroAleatorio);
+
+            Console.WriteLine("Adivina el numero");
+            // Almacenamos la respuesta del usuario y convertimos el string a int
+            int respuesta = Int32.Parse(Console.ReadLine());           
         }
     }
 }
